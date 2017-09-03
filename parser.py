f1d4ef1e981c5bdfcb186f1b9a9a359f3913fa6c
Tiago Katcipis <tiagokatcipis@gmail.com>
2017-09-03 20:14:16 -0300

    Add basic types

--- parser.py
+++ parser.py
@@ -1,11 +1,27 @@
 from collections import namedtuple
 
-BenchmarkResult = namedtuple('BenchmarkResult', ['chunk_size_kb'])
+BenchmarkResult = namedtuple(
+    'BenchmarkResult',
+    ['chunk_size_kb', 'blocksize_tests'],
+)
+
+BlockSizeTestResult = namedtuple(
+    'BlockSizeTestResult',
+    ['blocksize_kb', 'results'],
+)
+
+OperationTestResult = namedtuple(
+    'OperationTestResult',
+    ['operation', 'latency_ms', 'throughput_kbs'],
+)
+
+Latency = namedtuple( 'Latency', ['min', 'max', 'avg'])
 
 def parse(rawdata):
     lines = rawdata.readlines()
     chunk_size, lines = _parse_chunk_size(lines)
-    return BenchmarkResult(chunk_size_kb=chunk_size)
+    blocksize_tests = _parse_blocksize_tests(lines)
+    return BenchmarkResult(chunk_size_kb=chunk_size, blocksize_tests=blocksize_tests)
 
 def _parse_chunk_size(lines):
     chunksize_prefix = "RAID chunk size (KB):"
@@ -17,3 +33,6 @@ def _parse_chunk_size(lines):
         return int(parsed_chunk_size[1]), lines[i+1:]
 
     raise Exception("Unable to locate chunk size prefix on benchmark log")
+
+def _parse_blocksize_tests(lines):
+    return []
--- parser_test.py
+++ parser_test.py
@@ -11,22 +11,22 @@ class ParserTests(unittest.TestCase):
             parsed_chunk_test = parser.parse(rawdata)
             self.assertEqual(512, parsed_chunk_test.chunk_size_kb)
             self.assertEqual([
-                BlockSizeTest(blocksize_kb=8, results=[
-                        OperationTestResult(
+                parser.BlockSizeTestResult(blocksize_kb=8, results=[
+                        parser.OperationTestResult(
                             operation="sequential write",
-                            latency_ms=Latency(min=1.0,max=299.0,avg=2.45),
+                            latency_ms=parser.Latency(min=1.0,max=299.0,avg=2.45),
                             throughput_kbs=325885,
                         ),
-                        OperationTestResult(
+                        parser.OperationTestResult(
                             operation="sequential read",
-                            latency_ms=Latency(min=0.0, max=369016.0, avg=1546.10),
+                            latency_ms=parser.Latency(min=0.0, max=369016.0, avg=1546.10),
                             throughput_kbs=516940,
                         ),
                 ]),
-                BlockSizeTest(blocksize_kb=16, results=[
-                        OperationTestResult(
+                parser.BlockSizeTestResult(blocksize_kb=16, results=[
+                        parser.OperationTestResult(
                             operation="sequential write",
-                            latency_ms=Latency(min=1.0, max=309.0, avg=2.46),
+                            latency_ms=parser.Latency(min=1.0, max=309.0, avg=2.46),
                             throughput_kbs=650900,
                         ),
                 ]),
